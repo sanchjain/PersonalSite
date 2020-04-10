dc03ee0260fa019a49fdcf39588f22ab9becbb70
sanchjain <sanchjain101@gmail.com>
2020-04-10 22:14:10 +0530

    middleware

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,4 +135,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MIDDLEWARE = [
   'django.middleware.security.SecurityMiddleware',
   'whitenoise.middleware.WhiteNoiseMiddleware',
+'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
 ]
