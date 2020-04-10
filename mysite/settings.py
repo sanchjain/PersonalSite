1ea126f30d5244230546e4d9c89292a7e40fa134
sanchjain <sanchjain101@gmail.com>
2020-04-10 22:53:38 +0530

    postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,9 +77,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
+
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
